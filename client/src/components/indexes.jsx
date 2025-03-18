38b39b421b5bf7e61d4bf2f84602b81b19952fec
Mugole Joel <mugolejoel74@gmail.com>
2025-03-18 20:57:28 +0300

    setting routes

---
+++ client/src/components/indexes.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Maincontent from "./Maincontent";
+import Loader from "./Loader";
+import Loading from "./Loading";
+import ProtectedRoute from "./ProtectedRoute"
+
+
+
+
+export{Navbar,Sidebar,Maincontent,Loader,Loading,ProtectedRoute};
