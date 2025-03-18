338240b0ccbe613226d3db9e0d23a799071c5aed
shadiah-nalubega <shadiaahmed1302@gmail.com>
2025-03-18 20:54:58 +0300

    changed navbar colour

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,9 +3,12 @@ import { FaSearch, FaBell, FaCog } from "react-icons/fa";
 
 function Navbar() {
   return (
-    <nav className="bg-gray-900 text-gray-700 p-4 shadow-md">
+    <nav className="bg-gray-300  p-4 shadow-md">
       <div className="container mx-auto flex items-center justify-between">
-        <div className="text-white font-semibold text-lg">Username</div>
+        <div className="flex flex-col">
+          <span className="text-gray-800 font-bold text-lg">USERNAME</span>
+          <span className="text-sm text-gray-500">Tue, 27 June 2024</span>
+        </div>
         <div className="flex items-center space-x-4">
           <input
             type="text"
@@ -14,16 +17,19 @@ function Navbar() {
             className="bg-white outline-none text-white placeholder-gray-400 pl-8 pr-4 py-2 w-full rounded-lg"
           />
           <button>
-            <FaSearch className="w-6 h-6 text-black " />
+            <FaSearch className="w-6 h-6 text-gray-500 " />
           </button>
           {/* //absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400" size={18} */}
         </div>
         <div>
+          <p>Profile</p>
+        </div>
+        <div>
           <button>
-            <FaBell className="w-6 h-6 text-white" />
+            <FaBell className="w-6 h-6 text-black" />
           </button>
           <button>
-            <FaCog className="w-6 h-6 text-white" />
+            <FaCog className="w-6 h-6 text-black" />
           </button>
         </div>
       </div>
