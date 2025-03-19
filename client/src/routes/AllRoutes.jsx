18e063442e234be4aa23cc880fa66792c67b222a
shadiah-nalubega <shadiaahmed1302@gmail.com>
2025-03-19 13:48:42 +0300

    Updating Dashboard routing

--- client/src/routes/AllRoutes.jsx
+++ client/src/routes/AllRoutes.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import { Dashboard, Issues, Resolved, Reports, Settings } from "../pages";
 
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/" element={<Layout />} Navigate={"/dashboard"} >
+        <Route path="studentdashboard" element={<Dashboard />} />
         <Route index element={<Dashboard />} />
         <Route path="issues" element={<Issues />} />
         <Route path="resolved" element={<Resolved />} />
