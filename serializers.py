a166ac3a1827e7be4cf86c96e68a653db99bf7bb
patriciananswa <patriciananswa@gmail.com>
2025-03-23 22:27:05 +0300

    Merge branch 'patricia' of https://github.com/Suubi08/academic_issue_tracking_system into patricia

--- serializers.py
+++ serializers.py
@@ -1,27 +1,40 @@
 from rest_framework import serializers
-from .models import User, Issue, Notification, Comment, Attachment
+from .models import User, Issue, Comment, Notification, Attachment
 
+# User Serializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'username', 'email', 'role']
 
+# Issue Serializer
 class IssueSerializer(serializers.ModelSerializer):
+    student = UserSerializer(read_only=True)
+    assigned_to = UserSerializer(read_only=True)
+
     class Meta:
         model = Issue
-        fields = '__all__'
+        fields = ['id', 'title', 'description', 'category', 'status', 'created_at', 'student', 'assigned_to']
 
-class NotificationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Notification
-        fields = '__all__'
-
+# Comment Serializer
 class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
     class Meta:
         model = Comment
-        fields = '__all__'
+        fields = ['id', 'message', 'created_at', 'user', 'issue']
 
+# Notification Serializer
+class NotificationSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Notification
+        fields = ['id', 'message', 'status', 'created_at', 'user']
+
+# Attachment Serializer
 class AttachmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attachment
-        fields = '__all__'
+        fields = ['id', 'file', 'upload_at', 'issue']
+
