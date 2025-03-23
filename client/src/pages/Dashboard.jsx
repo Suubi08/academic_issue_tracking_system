85aa95f7663e222a4d5353cd48363780e6dac573
shadiah-nalubega <shadiaahmed1302@gmail.com>
2025-03-23 12:33:30 +0300

    building admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,52 @@
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card";
+import { AlertCircle, Bell, MessageSquare, Users } from "lucide-react";
 
 const Dashboard = () => {
+  const summaryItems = [
+    {
+      title: "Active Issues",
+      value: 8,
+      description: "Across all categories",
+      icon: AlertCircle,
+      Color: "text-red-500",
+      linkText: "View more",
+    },
+    {
+      title: "Assigned Issues",
+      value: "123",
+      description: "To lecturers",
+      icon: Users,
+      Color: "text-blue-500",
+      linkText: "View more",
+    },
+    {
+      title: "Recent Comments",
+      value: "45",
+      description: "Last 24 hours",
+      icon: MessageSquare,
+      Color: "text-green-500",
+      linkText: "View more",
+    },
+    {
+      title: "Pending Notifications",
+      value: "12",
+      description: "Unread",
+      icon: Bell,
+      Color: "text-yellow-500",
+      linkText: "View more",
+    },
+  ];
   return (
-    <div className="bg-yellow-200 text-center text-3xl font-bold">
-      Dashboard
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+      {summaryItems.map((items) => (
+        <li>{items.title}</li>
+      ))}
     </div>
   );
 };
