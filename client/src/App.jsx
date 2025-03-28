1a57ee90ee69233730567cc6936c8c7cc2d05f26
Mugole Joel <mugolejoel74@gmail.com>
2025-03-28 18:27:18 +0300

    issue report

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,10 @@ import AllRoutes from "./routes/AllRoutes";
 import { Issuetable } from "./components";
 import Test from "./components/Test";
 import GoodCard from './components/GoodCard'
-import IssueTracking from "./components/IssueTracking";
 import Auth from "./components/Auth"
+import Button from "./components/ui/Button";
+import Textarea from "./components/ui/Textarea";
+import Input from "./components/ui/Input";
 
 const App = () => {
   return (
@@ -12,8 +14,15 @@ const App = () => {
       {/* <GoodCard/> */}
       {/* <Test/> */}
       {/* <AllRoutes /> */}
+      {/* <div className="p-4 space-x-2">
+      <Button variant="default" size="lg">Default Button</Button>
+      <Button variant="outline">Outline Button</Button>
+      <Button variant="destructive" size="sm">Destructive Button</Button>
+    </div> */}
+{/* <Textarea/> */}
+<Input type='text'  placeholder='enter '/>
       {/* <IssueTracking/> */}
-      <Auth/>
+      {/* <Auth/> */}
     </div>
   );
 };
