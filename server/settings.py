1269dc8508cabc35adb40112aeeb9c3d4cdd54da
Rosemary-n <rose.nakitto@gmail.com>
2025-03-30 08:45:06 +0300

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -124,3 +124,26 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT _AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framwork_simplejwt.authentication.JWTAuthentication',
+    ),
+  
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),    
+}
+
+AUTH_USER_MODEL = 'api.User'
+
+    
+
+
+
