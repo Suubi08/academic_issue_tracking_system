c2b549359eaa3432028a2bd11cf59bde095d2d0e
Rosemary-n <rose.nakitto@gmail.com>
2025-03-30 14:07:26 +0300

    Update urls.py

--- server/backend/api/urls.py
+++ server/backend/api/urls.py
@@ -3,10 +3,11 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterView, LoginView, RefreshTokenView
 from .views import UserListView, IssueListView, IssueDetailView, NotificationListView
 
+# Listing all users
 urlpatterns = [
     path('users/', UserListView.as_view(), name='user-list'),
     path('issues/', IssueListView.as_view(), name='issue-list'),
-    path('issues/<int:pk>/', IssueDetailView.as_view(), name='issue-detail'),
+    path('issues/<int:pk>/', IssueDetailView.as_view(), name='issue-detail'), #Endpoint for retrieving, updating, and deleting issue by a pk
     path('notifications/', NotificationListView.as_view(), name='notification-list'),
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
