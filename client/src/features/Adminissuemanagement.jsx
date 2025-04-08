679ab1e77e93b640d699c9368a90263716fe8a5e
shadiah-nalubega <shadiaahmed1302@gmail.com>
2025-04-08 20:20:05 +0300

    Update Adminissuemanagement.jsx

--- client/src/features/Adminissuemanagement.jsx
+++ client/src/features/Adminissuemanagement.jsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 import {
   Card,
   CardHeader,
-  CardFooter,
+ 
   CardTitle,
-  CardDescription,
+ 
   CardContent,
 } from "../components/ui"
 import {
