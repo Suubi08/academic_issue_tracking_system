15978cc2e9022786176dca628ab3398f2ab9f9d0
patriciananswa <nanswapatricia8@gmail.com>
2025-04-09 20:36:17 +0300

    Update signals.py

--- server/backend/api/signals.py
+++ server/backend/api/signals.py
@@ -22,38 +22,3 @@ def assign_user_group(sender, instance, created, **kwargs):
             instance.groups.add(group) #Assign the user to the correct group
 
 
-@receiver(post_save, sender=Issue)
-def send_assignment_email(sender, instance, created, **kwargs):
-    """
-    Send an email when an issue is assigned to a user.
-    """
-    if instance.assigned_to:  # Only send if someone is assigned
-
-        subject = "New Issue Assigned"
-
-        message = f"""
-        Hello {instance.assigned_to.first_name},
-
-        A new issue has been assigned to you on the Academic Issue Tracking System.
-
-        Category: {instance.category}
-        Description: {instance.description}
-        Status: {instance.status.capitalize()}
-        Created By: {instance.created_by.username}
-
-        Please log into your dashboard to take action.
-
-        Regards,
-        AITS Team
-        """
-
-        recipient_email = instance.assigned_to.email
-
-        # Send the email notification
-        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [recipient_email])
-
-        # Create a dashboard notification
-        Notification.objects.create(
-            user=instance.assigned_to,
-            message=f"You have been assigned a new issue."
-        )
