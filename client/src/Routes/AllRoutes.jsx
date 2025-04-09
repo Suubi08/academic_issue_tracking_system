5d251d0716fcab96569be802bf6d9775d2bf2b1c
MUGOLE JOEL <joelmugole74@gmail.com>
2025-04-09 20:16:15 +0300

    restructring the routes

--- client/src/Routes/AllRoutes.jsx
+++ client/src/Routes/AllRoutes.jsx
@@ -18,6 +18,7 @@ import {
   LecturerDashboard,
   PageNotFound,
   Studentdashboard,
+  RegistrarDashboard
 } from "../pages";
 
 import {
@@ -33,7 +34,10 @@ import {
   Studentissues,
   Studentsubmitissue,
   Studentsettings,
+  RegistrarNotifications,
+  RegistrarSettings,
 } from "../features";
+
 
 const AllRoutes = () => {
   return (
@@ -45,67 +49,3 @@ const AllRoutes = () => {
       {/* Redirect root to login if not authenticated */}
       <Route path="/" element={<Navigate to="/login" replace />} />
 
-      {/* Student Routes */}
-      <Route element={<ProtectedRoute allowedRoles={["student"]} />}>
-        <Route element={<Layout />}>
-          <Route path="/student-dashboard" element={<Studentdashboard />} />
-          <Route path="/studentissues" element={<Studentissues />} />
-          <Route path="/submitissue" element={<Studentsubmitissue />} />
-          <Route path="/viewissue/:id" element={<IssueDetail />} />
-          {/* <Route path="/studentissuereport" element={<Studentissuereport />} /> */}
-          <Route path="/studentsettings" element={<Studentsettings />} />
-        </Route>
-      </Route>
-
-      {/* Lecturer Routes */}
-      <Route element={<ProtectedRoute allowedRoles={["lecturer"]} />}>
-        <Route element={<Layout />}>
-          <Route path="/lecturer-dashboard" element={<LecturerDashboard />} />
-          <Route
-            path="/issuemanagement"
-            element={<Lecturerissuemanagement />}
-          />
-          <Route path="/status-update" element={<Lecturerstatusupdates />} />
-          <Route
-            path="/lecturernotifications"
-            element={<LecturerNotifications />}
-          />
-          <Route path="/lecturersettings" element={<Lecturersettings />} />
-        </Route>
-      </Route>
-
-      {/* Admin Routes */}
-      <Route element={<ProtectedRoute allowedRoles={["admin"]} />}>
-        <Route element={<Layout />}>
-          <Route path="/admin-dashboard" element={<AdminDashboard />} />
-          <Route
-            path="/adminissuemanagement"
-            element={<Adminissuemanagement />}
-          />
-          <Route
-            path="/adminusermanagement"
-            element={<Adminusermanagement />}
-          />
-          <Route path="/adminNotifications" element={<AdminNotifications />} />
-          <Route path="/adminreports" element={<AdminReports />} />
-          <Route path="/adminsettings" element={<Adminsettings />} />
-        </Route>
-      </Route>
-
-      {/* Academic Registrar Routes
-      <Route element={<ProtectedRoute allowedRoles={["academic_registrar"]} />}>
-        <Route element={<Layout />}>
-          <Route path="/registrar-dashboard" element={<AdminDashboard />} />
-          <Route path="/issues" element={<Issues />} />
-          <Route path="/reports" element={<Reports />} />
-          <Route path="/settings" element={<Settings />} />
-        </Route>
-      </Route> */}
-
-      {/* Catch-all 404 route */}
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
-  );
-};
-
-export default AllRoutes;
