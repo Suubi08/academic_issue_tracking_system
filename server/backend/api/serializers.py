2e50aa21962e83ad22bc6a72575c13e9862d51bd
Rosemary-n <rose.nakitto@gmail.com>
2025-04-10 15:04:19 +0300

    Update serializers.py

--- server/backend/api/serializers.py
+++ server/backend/api/serializers.py
@@ -14,14 +14,11 @@ class RegisterSerializer(serializers.ModelSerializer):
             'student_number', 'course_name', 'college', 
             'lecture_number', 'subject_taught', 'department'
         ]
-        extre_kwargs = {
+                                             
+        extra_kwargs = {
             'password': {'write_only': True},
-            'confirm_password': {'write_only": True)
-            }                                        
-        # extra_kwargs = {
-        #     'password': {'write_only': True},
-        #     'confirm_password': {'write_only': True}
-        # }
+            'confirm_password': {'write_only': True}
+        }
 
     def validate(self, data):
         if data['password'] != data['confirm_password']:
