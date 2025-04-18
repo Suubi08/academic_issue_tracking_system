61023c5f295cd146a73c0314d3da5a106f9b38f8
MUGOLE JOEL <joelmugole74@gmail.com>
2025-04-18 19:48:31 +0300

    importing card for Studentissuereport.jsx

--- client/src/features/Studentissuereport.jsx
+++ client/src/features/Studentissuereport.jsx
@@ -1,1 +1,11 @@
 
+import { useState } from "react";
+import { Paperclip } from "lucide-react";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "../components";
