6e838ab8a10deaa46f52fcab879438bdfb983db5
MUGOLE JOEL <joelmugole74@gmail.com>
2025-04-18 19:42:01 +0300

    importing for RegistrarSettings.jsx

--- client/src/features/RegistrarSettings.jsx
+++ client/src/features/RegistrarSettings.jsx
@@ -1,1 +1,27 @@
 
+import { Switch } from "../components/ui";
+import { Textarea } from "../components/ui";
+import {
+  Select,
+  SelectItem,
+  SelectContent,
+  SelectValue,
+  SelectTrigger,
+} from "../components/ui";
+import { Input } from "../components/ui";
+import { Label } from "../components/ui";
+import { Avatar, AvatarFallback, AvatarImage } from "../components/ui";
+import { useState } from "react";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "../components";
+import { Button } from "../components/ui";
+
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "../components/ui";
+
+import { User, Bell, Shield, Key } from "lucide-react";
