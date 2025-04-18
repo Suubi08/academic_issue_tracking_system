b78e351af88c1e484b24a00f79595838600f291e
MUGOLE JOEL <joelmugole74@gmail.com>
2025-04-18 19:49:52 +0300

    implementing constants Studentissuereport.jsx

--- client/src/features/Studentissuereport.jsx
+++ client/src/features/Studentissuereport.jsx
@@ -21,3 +21,31 @@ import {
 import { Label } from "../components/ui/Label";
 import { useNavigate } from "react-router-dom";
 import { Input } from "../components/ui/Input";
+
+
+const categories = [
+  { value: "marks", label: "Missing marks" },
+  { value: "appeals", label: "Appeals" },
+  { value: "corrections", label: "Corrections" },
+];
+
+const courseUnits = [
+  { value: "OS", label: "Operating Systems" },
+  { value: "DSA", label: "Data Structures and Algorithms" },
+  { value: "SAD", label: "System Analysis and Design" },
+  { value: "SD", label: "Software Development" },
+  { value: "PS", label: "Probability and Statistics" },
+];
+
+const yearsOfStudy = [
+  { value: "2025/2026", label: "2025/2026" },
+  { value: "2024/2025", label: "2024/2025" },
+  { value: "2023/2024", label: "2023/2024" },
+  { value: "2022/2023", label: "2022/2023" },
+  { value: "2021/2022", label: "2021/2022" },
+];
+
+const semesters = [
+  { value: "one", label: "Semester One" },
+  { value: "two", label: "Semester Two" },
+];
