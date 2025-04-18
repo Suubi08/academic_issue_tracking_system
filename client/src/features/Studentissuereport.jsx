f045752024c2dcccab09e1c93925d9a448bc9a86
MUGOLE JOEL <joelmugole74@gmail.com>
2025-04-18 19:48:57 +0300

    importing button ,textarere Studentissuereport.jsx

--- client/src/features/Studentissuereport.jsx
+++ client/src/features/Studentissuereport.jsx
@@ -9,3 +9,12 @@ import {
   CardDescription,
   CardContent,
 } from "../components";
+import { Button } from "../components/ui/Button";
+import { Textarea } from "../components/ui/Textarea";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../components/ui/select";
