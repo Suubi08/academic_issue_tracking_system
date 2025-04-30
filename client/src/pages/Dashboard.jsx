b717de2bd63ae36011c75aae65e026d1f944fe35
MUGOLE JOEL <joelmugole74@gmail.com>
2025-04-30 08:33:12 +0300

    Update Dashboard.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 const Dashboard = () => {
-  return <div className="bg-yellow-200 text-center text-3xl font-bold">Dashboard</div>
+  return <div className="bg-yellow-200 text-center text-3xl font-bold">Dashboard demo </div>
 }
 
 export default Dashboard
