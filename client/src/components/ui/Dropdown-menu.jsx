a570dc6a01b8d9b7de9290f74a0eb7ea073fbcc3
Rosemary-n <rose.nakitto@gmail.com>
2025-05-01 23:07:10 +0300

    Update Dropdown-menu.jsx

--- client/src/components/ui/Dropdown-menu.jsx
+++ client/src/components/ui/Dropdown-menu.jsx
@@ -18,7 +18,7 @@ const DropdownMenuSubTrigger = ({ className, inset, children, ...props }) => (
       className,
     )}
     {...props}
-  >
+  > 
     {children}
     <ChevronRight className="ml-auto" />
   </DropdownMenuPrimitive.SubTrigger>
@@ -34,18 +34,18 @@ const DropdownMenuSubContent = ({ className, ...props }) => (
   />
 )
 
-const DropdownMenuContent = ({ className, sideOffset = 4, ...props }) => (
-  <DropdownMenuPrimitive.Portal>
-    <DropdownMenuPrimitive.Content
-      sideOffset={sideOffset}
-      className={cn(
-        "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-        className,
-      )}
-      {...props}
-    />
-  </DropdownMenuPrimitive.Portal>
-)
+// const DropdownMenuContent = ({ className, sideOffset = 4, ...props }) => (
+//   <DropdownMenuPrimitive.Portal>
+//     <DropdownMenuPrimitive.Content
+//       sideOffset={sideOffset}
+//       className={cn(
+//         "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+//         className,
+//       )}
+//       {...props}
+//     />
+//   </DropdownMenuPrimitive.Portal>
+// )
 
 const DropdownMenuItem = ({ className, inset, ...props }) => (
   <DropdownMenuPrimitive.Item
